d83dab7c31ee63aa37a13abafd71b7782b98a40e
Brian Allen <brianallen2401@gmail.com>
2020-02-28 13:16:52 -0700

    added and tested spotify call. mostly working? added error call as well to write to log.txt

--- liri.js
+++ liri.js
@@ -1,7 +1,25 @@
 require("dotenv").config();
 
 const keys = require("./keys.js");
+const Spotify = require("node-spotify-api");
+const axios = require("axios");
+const fs = require("fs");
 const spotify = new Spotify(keys.spotify);
+const userInput = process.argv.slice(2);
+const userCommand = userInput.process.argv[0];
+const userParameter = userInput.process.argv[1];
 
-const userCommand = process.argv.slice(2);
+fs.appendFile('log.txt', userCommand + ",", function(err) {
+    if (err) {
+        console.log(err);
+    }
+})
 
+spotify
+    .search({ type: 'track', query: 'All the Small Things', limit: 5 })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
